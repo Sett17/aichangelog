875147345265a590c77e70fe47c84a417d4693c5
sett <raik.rohde@gmail.com>
2023-04-05 22:48:49 +0200

    feat: add OpenAI integration for generating changelogs
    This commit adds integration with the OpenAI API to generate chang

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,182 @@
+use std::{env, process, time::Duration};
+
 use clap::Parser;
+use colored::Colorize;
+use crossterm::{
+    cursor::{self, MoveToColumn, MoveToPreviousLine},
+    execute,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    terminal::{self, Clear, ClearType},
+};
+use futures::stream::StreamExt;
+use reqwest_eventsource::{Event, EventSource};
+use unicode_segmentation::UnicodeSegmentation;
 
-fn main() {
+use crate::openai::Message;
+
+mod openai;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let Ok(api_key) = env::var("OPENAI_API_KEY") else {
+        println!("{} {}", "OPENAI_API_KEY not set.".red(), "Refer to step 3 here: https://help.openai.com/en/articles/5112595-best-practices-for-api-key-safety".bright_black());
+        process::exit(1);
+    };
+
     let args = Args::parse();
-    println!("{:?}", args);
 
-    let repo = git2::Repository::open(".").unwrap();
-    let mut revwalk = repo.revwalk().unwrap();
-    revwalk.push_range(&args.range).unwrap();
-
-    for oid in revwalk {
-        let commit = repo.find_commit(oid.unwrap()).unwrap();
-        let message = commit.message().unwrap();
-        let message = if args.short {
-            message.lines().next().unwrap()
-        } else {
-            message
-        };
-        println!("{} {}", commit.id(), message);
+    let mut cmd = process::Command::new("git");
+    cmd.arg("log");
+    if args.short {
+        cmd.arg("--oneline");
     }
+    if let Some(range) = args.range {
+        cmd.arg(range);
+    }
+    let output = match cmd.output() {
+        Ok(output) => String::from_utf8(output.stdout).expect("Failed to parse output"),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let prompt_tokens = openai::count_token(&output)?;
+    if prompt_tokens > args.model.context_size() {
+        eprintln!(
+            "Error: Git log is too long. Prompt is {} tokens, but the maximum is {}.\nTry using a smaller range or the -s flag.",
+            format!("{}", prompt_tokens).purple(),
+            format!("{}", args.model.context_size()).purple()
+        );
+        process::exit(1);
+    }
+
+    let messages = vec![
+        Message::system(String::from(SYSTEM_MSG)),
+        Message::user(output),
+    ];
+
+    let req = openai::Request::new(
+        args.model.clone().to_string(),
+        messages,
+        1,
+        args.temp,
+        args.freq,
+    );
+
+    let json = match serde_json::to_string(&req) {
+        Ok(json) => json,
+        Err(e) => {
+            println!("{e}");
+            process::exit(1);
+        }
+    };
+
+    let request_builder = reqwest::Client::new()
+        .post("https://api.openai.com/v1/chat/completions")
+        .header("Content-Type", "application/json")
+        .bearer_auth(api_key)
+        .body(json);
+
+    let loading_ai_animation = tokio::spawn(async {
+        let emoji_support =
+            terminal_supports_emoji::supports_emoji(terminal_supports_emoji::Stream::Stdout);
+        let frames = if emoji_support {
+            vec![
+                "🕛", "🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚",
+            ]
+        } else {
+            vec!["/", "-", "\\", "|"]
+        };
+        let mut current_frame = 0;
+        let mut stdout = std::io::stdout();
+        loop {
+            current_frame = (current_frame + 1) % frames.len();
+            match execute!(
+                stdout,
+                Clear(ClearType::CurrentLine),
+                MoveToColumn(0),
+                SetForegroundColor(Color::Yellow),
+                Print("Asking AI "),
+                Print(frames[current_frame]),
+                ResetColor
+            ) {
+                Ok(_) => {}
+                Err(_) => {
+                    break;
+                }
+            }
+            tokio::time::sleep(Duration::from_millis(150)).await;
+        }
+    });
+
+    let term_width = terminal::size()?.0 as usize;
+
+    let mut stdout = std::io::stdout();
+
+    let mut changelog = String::new();
+
+    let mut es = EventSource::new(request_builder)?;
+    let mut lines_to_move_up = 0;
+    let mut response_tokens = 0;
+    while let Some(event) = es.next().await {
+        if !loading_ai_animation.is_finished() {
+            loading_ai_animation.abort();
+            execute!(
+                std::io::stdout(),
+                Clear(ClearType::CurrentLine),
+                MoveToColumn(0),
+            )?;
+            print!("\n\n")
+        }
+
+        execute!(
+            stdout,
+            cursor::SavePosition,
+            MoveToPreviousLine(lines_to_move_up),
+        )?;
+        lines_to_move_up = 0;
+        match event {
+            Ok(Event::Message(message)) => {
+                if message.data == "[DONE]" {
+                    break;
+                }
+                execute!(stdout, Clear(ClearType::FromCursorDown),)?;
+                let resp = serde_json::from_str::<openai::Response>(&message.data)
+                    .map_or_else(|_| openai::Response::default(), |r| r);
+                if let Some(delta) = &resp.choices[0].delta.content {
+                    changelog.push_str(delta);
+                    response_tokens += 1;
+                }
+                let outp = format!(
+                    "{}{}\n{}\n",
+                    Print(format!("{}\n", "=======================").bright_black()),
+                    format!(
+                        "This used {} tokens costing you about {}\n",
+                        format!("{}", response_tokens + prompt_tokens).purple(),
+                        format!("~${:0.4}", args.model.cost(prompt_tokens, response_tokens))
+                            .purple()
+                    ),
+                    changelog,
+                );
+                print!("{outp}");
+                lines_to_move_up += count_lines(&outp, term_width) - 1;
+            }
+            Err(e) => {
+                println!("{e}");
+                process::exit(1);
+            }
+            _ => {}
+        }
+    }
+
+    execute!(
+        stdout,
+        cursor::RestorePosition,
+        Print(format!("{}\n", "=======================").bright_black()),
+    )?;
+
+    Ok(())
 }
 
 // tool to generate changelog from commit range
@@ -32,9 +191,52 @@ fn main() {
 #[command(author, version, about, long_about = None)]
 struct Args {
     ///Rev range to generate changelog from
-    range: String,
+    range: Option<String>,
 
     ///Only use first line of commit message to reduce tokens
     #[arg(short, long)]
     short: bool,
+
+    ///Temperature for AI
+    /// 0.0 - 2.0
+    #[arg(short, long, default_value = "1.0")]
+    temp: f64,
+
+    ///Frequency Penalty for AI
+    /// -2.0 - 2.0
+    #[arg(short, long, default_value = "0.0")]
+    freq: f64,
+
+    ///Model to use
+    #[arg(short, long, default_value = "gpt-3.5-turbo")]
+    model: openai::Model,
 }
+
+#[must_use]
+pub fn count_lines(text: &str, max_width: usize) -> u16 {
+    if text.is_empty() {
+        return 0;
+    }
+    let mut line_count = 0;
+    let mut current_line_width = 0;
+    for cluster in UnicodeSegmentation::graphemes(text, true) {
+        match cluster {
+            "\r" | "\u{FEFF}" => {}
+            "\n" => {
+                line_count += 1;
+                current_line_width = 0;
+            }
+            _ => {
+                current_line_width += 1;
+                if current_line_width > max_width {
+                    line_count += 1;
+                    current_line_width = cluster.chars().count();
+                }
+            }
+        }
+    }
+
+    line_count + 1
+}
+
+const SYSTEM_MSG: &str = r#"You are now an AI that takes a range of Git commit messages as input and generates a changelog in the style of update notes using Markdown formatting. The commit messages may be in the format of a one-line summary or a multi-line description."#;
---
+++ src/openai.rs
@@ -0,0 +1,202 @@
+#![allow(dead_code)]
+
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum Role {
+    System,
+    User,
+    Assistant,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Message {
+    pub role: Role,
+    pub content: String,
+}
+
+impl Message {
+    pub const fn system(content: String) -> Self {
+        Self {
+            role: Role::System,
+            content,
+        }
+    }
+    pub const fn user(content: String) -> Self {
+        Self {
+            role: Role::User,
+            content,
+        }
+    }
+    pub const fn assistant(content: String) -> Self {
+        Self {
+            role: Role::Assistant,
+            content,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ErrorRoot {
+    pub error: Error,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Error {
+    pub message: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    pub param: Option<String>,
+    pub code: Option<String>,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} ({:?}): {:?}",
+            self.type_field, self.code, self.message
+        )
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Request {
+    pub model: String,
+    pub messages: Vec<Message>,
+    pub n: i32,
+    pub temperature: f64,
+    pub frequency_penalty: f64,
+    stream: bool,
+}
+
+impl Request {
+    pub fn new(
+        model: String,
+        messages: Vec<Message>,
+        n: i32,
+        temperature: f64,
+        frequency_penalty: f64,
+    ) -> Self {
+        Self {
+            model,
+            messages,
+            n,
+            temperature,
+            frequency_penalty,
+            stream: true,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Response {
+    pub id: String,
+    pub object: String,
+    pub created: i64,
+    pub model: String,
+    pub choices: Vec<Choice>,
+    pub usage: Option<Usage>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Choice {
+    pub index: i64,
+    pub finish_reason: Option<String>,
+    pub delta: Delta,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct Delta {
+    pub role: Option<Role>,
+    pub content: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Usage {
+    pub prompt_tokens: usize,
+    pub completion_tokens: usize,
+    pub total_tokens: usize,
+}
+
+pub fn count_token(s: &str) -> anyhow::Result<usize> {
+    let bpe = tiktoken_rs::cl100k_base()?;
+    let tokens = bpe.encode_with_special_tokens(s);
+    Ok(tokens.len())
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Model {
+    #[default]
+    Gpt35Turbo,
+    Gpt4,
+    Gpt432k,
+}
+
+impl FromStr for Model {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "gpt-3.5-turbo" => Ok(Self::Gpt35Turbo),
+            "gpt-4" => Ok(Self::Gpt4),
+            "gpt-4-32k" => Ok(Self::Gpt432k),
+            _ => Err(format!("{} is not a valid model", s)),
+        }
+    }
+}
+
+impl ToString for Model {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Gpt35Turbo { .. } => String::from("gpt-3.5-turbo"),
+            Self::Gpt4 { .. } => String::from("gpt-4"),
+            Self::Gpt432k { .. } => String::from("gpt-4-32k"),
+        }
+    }
+}
+
+impl Serialize for Model {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+impl<'de> Deserialize<'de> for Model {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Self::from_str(&s).map_err(serde::de::Error::custom)
+    }
+}
+
+impl Model {
+    pub fn cost(&self, prompt_tokens: usize, completion_tokens: usize) -> f64 {
+        let (prompt_cost, completion_cost) = match self {
+            Self::Gpt35Turbo => (0.002, 0.002),
+            Self::Gpt4 => (0.03, 0.06),
+            Self::Gpt432k => (0.06, 0.12),
+        };
+        (prompt_tokens as f64).mul_add(
+            prompt_cost / 1000.0,
+            (completion_tokens as f64) * (completion_cost / 1000.0),
+        )
+    }
+    pub const fn context_size(&self) -> usize {
+        match self {
+            Self::Gpt35Turbo => 4096,
+            Self::Gpt4 => 8192,
+            Self::Gpt432k => 32768,
+        }
+    }
+}
